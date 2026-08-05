ce02b14a332cb3ae57ebc0b07d890dcb7fd284bd
Saifalanii <saifalani1996@gmail.com>
2026-08-05 14:54:42 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //
 // Bump CACHE when shipping: the old cache is deleted on activate.
 
-const CACHE = 'nightfall-v1';
+const CACHE = 'nightfall-v2';
 
 // On localhost the cache-first strategy below happily serves the module you edited
 // thirty seconds ago, and you debug a file the page isn't running. Development gets
